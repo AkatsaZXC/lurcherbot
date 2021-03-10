5a8964cb1cbe4352a0117c2b726ee688b0e7437f
Leonid <akatsaaaaa@gmail.com>
2021-03-10 14:54:54 +0300

    now using email

--- main/main.ino
+++ main/main.ino
@@ -1,56 +1,64 @@
+#include <Arduino.h>
+#if defined(ESP32)
+#include <WiFi.h>
+#elif defined(ESP8266)
 #include <ESP8266WiFi.h>
-#include <WiFiClientSecure.h>
-#include <TelegramBot.h>
+#endif
+#include <ESP_Mail_Client.h>
 
-int switch_pin = 14; //  Example
+#define WIFI_SSID "SSID"
+#define WIFI_PASS "PASS"
 
-//  WiFi and TelegramBot settings
-const char* ssid = "SSID";
-const char* pass = "PASS";
-const char* BotToken[] = "bot_token";
+#define SMTP_SERVER "smtp.gmail.com"
+#define SMTP_PORT   465
 
-// Initializing TelegramBot
-WiFiClientSecure net_ssl;
-TelegramBot telegram_bot (BotToken, net_ssl);
+#define AUTHOR_EMAIL "EMAIL"
+#define AUTHOR_PASS  "PASS"
+
+SMTPSeddion smtp;
+
 
 void setup(){
-  Serial.begin(9600);
-  WiFi.begin(ssid, pass);
+  Serial.begin(115200);
+  Serial.println();
 
-// Checking WiFi connection
+  Serial.printt("Connecting to WiFi");
+
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
   while(WiFi.status() != WL_CONNECTED){
-    delay(500);
     Serial.print(".");
+    delay(200);
   }
 
-Serial.println("WiFi connected!");
-Serial.print("ESP8266 IP: ");
-Serial.println(WiFi.localIP());
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println(WiFi.localIP());
+  Serial.println();
 
-// Starting TelegramBot
-telegram_bot.begin();
+  smtp.debug(1); // 1- basic debug, 0 - no debug
+  //  smtpcallback
+
+  //  Enable SMTP session
+  ESP_Mail_Session session;
+
+  //  Session config
+  session.server.host_name = SMTP_HOST;
+  session.server.port = SMTP_PORT;
+  session.login.email = AUTHOR_EMAIL;
+  session.login.password = AUTHOR_PASSWORD;
+  session.login.user_domain = "mydomain.net";
+
+  //  Declare message class
+  SMTP_Message message;
+
+  //  Set the message headers
+  message.sender.name = "ESP MailTest";
+  message.sender.email = AUTHOR_EMAIL;
+  message.subject = "Test";
+  message.addRecipient("Test", "someone_email");
+
+  message.text.content = "Privet";
   
-}
-
-void loop(){
-
-  // Checking for new messages
-  message m = telegram_bot.getUpdates();
-
-  if(m.text.equals("wifi"))){
-    if(WiFi.status() = WL_CONNECTED){
-      bot.sendMessage(m.chat_id, "Yes, I am connected to");
-      bot.sendMessage(m.chat_id, ssid);
-    }else{
-      Serial.println("Not connected to WiFi");
-    }
-  }
-
-  int switch_value = digitalRead(switch_pin);
-
-  if(switch_value == HIGH){
-    bot.sendMessage(m.chat_id, "Door opened"); //  0 - closed, 1 - opened
-  }
-  
+  message.text.charSet = "us-ascii";
   
 }
