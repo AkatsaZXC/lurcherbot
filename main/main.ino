8a0726f0afa3bd33aaf10f71afc9f763b154a4ea
Leonid <akatsaaaaa@gmail.com>
2021-03-10 15:36:12 +0300

    main changes

--- main/main.ino
+++ main/main.ino
@@ -15,14 +15,15 @@
 #define AUTHOR_EMAIL "EMAIL"
 #define AUTHOR_PASS  "PASS"
 
-SMTPSeddion smtp;
+SMTPSession smtp;
 
+void smtpCallback(SMTP_Status status);
 
 void setup(){
   Serial.begin(115200);
   Serial.println();
 
-  Serial.printt("Connecting to WiFi");
+  Serial.print("Connecting to WiFi");
 
   WiFi.begin(WIFI_SSID, WIFI_PASS);
   while(WiFi.status() != WL_CONNECTED){
@@ -36,29 +37,76 @@ void setup(){
   Serial.println();
 
   smtp.debug(1); // 1- basic debug, 0 - no debug
-  //  smtpcallback
+
+  smtp.callback(smtpCallback);
 
   //  Enable SMTP session
   ESP_Mail_Session session;
 
   //  Session config
-  session.server.host_name = SMTP_HOST;
+  session.server.host_name = SMTP_SERVER;
   session.server.port = SMTP_PORT;
   session.login.email = AUTHOR_EMAIL;
-  session.login.password = AUTHOR_PASSWORD;
-  session.login.user_domain = "mydomain.net";
+  session.login.password = AUTHOR_PASS;
+  //session.login.user_domain = "mydomain.net";
 
   //  Declare message class
   SMTP_Message message;
 
   //  Set the message headers
-  message.sender.name = "ESP MailTest";
+  message.sender.name = "Name";
   message.sender.email = AUTHOR_EMAIL;
-  message.subject = "Test";
-  message.addRecipient("Test", "someone_email");
+  message.subject = "Subject";
+  message.addRecipient("Test", "someone_mail");
 
-  message.text.content = "Privet";
+  message.text.content = "Pidoras";
   
   message.text.charSet = "us-ascii";
-  
+
+  message.text.transfer_encoding = Content_Transfer_Encoding::enc_7bit;
+
+  message.priority = esp_mail_smtp_priority::esp_mail_smtp_priority_high;
+
+  message.response.notify = esp_mail_smtp_notify_success | esp_mail_smtp_notify_failure | esp_mail_smtp_notify_delay;
+
+  if (!smtp.connect(&session))
+    return;
+
+  /* Start sending Email and close the session */
+  if (!MailClient.sendMail(&smtp, &message))
+    Serial.println("Error sending Email, " + smtp.errorReason());
+}
+
+void loop()
+{
+}
+
+void smtpCallback(SMTP_Status status)
+{
+  /* Print the current status */
+  Serial.println(status.info());
+
+  /* Print the sending result */
+  if (status.success())
+  {
+    Serial.println("----------------");
+    Serial.printf("Message sent success: %d\n", status.completedCount());
+    Serial.printf("Message sent failled: %d\n", status.failedCount());
+    Serial.println("----------------\n");
+    struct tm dt;
+
+    for (size_t i = 0; i < smtp.sendingResult.size(); i++)
+    {
+      /* Get the result item */
+      SMTP_Result result = smtp.sendingResult.getItem(i);
+      localtime_r(&result.timesstamp, &dt);
+
+      Serial.printf("Message No: %d\n", i + 1);
+      Serial.printf("Status: %s\n", result.completed ? "success" : "failed");
+      Serial.printf("Date/Time: %d/%d/%d %d:%d:%d\n", dt.tm_year + 1900, dt.tm_mon + 1, dt.tm_mday, dt.tm_hour, dt.tm_min, dt.tm_sec);
+      Serial.printf("Recipient: %s\n", result.recipients);
+      Serial.printf("Subject: %s\n", result.subject);
+    }
+    Serial.println("----------------\n");
+  }
 }
